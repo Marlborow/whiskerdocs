9ac7d982c159ba2118ac2c67b295d0e6da231f9c
Sirrus <60309425+Marlborow@users.noreply.github.com>
2025-04-09 16:33:29 +0100

    Removed unused log function.

--- src/parser.lua
+++ src/parser.lua
@@ -1,11 +1,5 @@
 local M = {}
 function M.run(line, plugins)
-
-    local function log(plugin, extra)
-        if not plugin then return end
-        print("[Interpreter]: " .. (plugin.__name or "unknown") .. extra)
-    end
-
     local result = line
 
     for _,loaded in ipairs(plugins) do
